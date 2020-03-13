5f71598908e000bedbc15c64a5dee0152c097f6d
mvkras <avkras1992@gmail.com>
2020-03-13 22:25:20 +1000

    Started OOP, params

---
+++ D_OOP/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_OOP
+{
+   class Character  //Заполняем данные класса
+    {
+      int health = 100; //поле здоровье
+        //Объявляем свойства для чтения полей {get; set}. Свойства пишутся с public. Свойство - это оболочка над полем     
+        public int GetHealth() //Свойство/метод для чтения поля здоровье, чтобы его не могли изменить
+        {
+            return health;
+        }
+        private void SetHealth(int value) //Записывать значение не на прямую. Для защиты
+        {
+            health = value;
+        }
+        public void Hit(int damage)//поведение класса задается методами. Void - пустота
+        {
+            if(damage > GetHealth())//Чтобы здоровье не было отрицательным
+            {
+               damage = GetHealth(); //Если в damage большее значение, чем здоровья у игрока, то эта разница передается в damage, а health = 0;
+            }
+           var health = GetHealth() - damage; //Уменьшает величину здоровья в зависимости от урона
+            SetHealth(health);
+        }
+
+    }
+}
---
+++ D_OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace D_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character warrior = new Character(); //У каждого класса свое предназначение, Класс кошка не может создавать ракеты...
+            warrior.Hit(90);
+            Console.WriteLine($"Здоровье персонажа: {warrior.GetHealth()}");
+            Calculator calc = new Calculator();           
+            double result = calc.SqrtLength(8, 12, 45);
+            result = Math.Round(result, 2);
+            Console.WriteLine("Площадь равна: "+result);
+            
+           double avg = calc.Average(new int[] { 1, 2, 3, 4 }); //инициализация массива Average
+            Console.WriteLine("Среднее значение: "+avg);
+            //С помощью params можно передавать значения массива через запятую
+            double avg2 = calc.Average2( 1, 2, 3, 4 ); //инициализация массива Average. 
+
+        }
+    }
+}
